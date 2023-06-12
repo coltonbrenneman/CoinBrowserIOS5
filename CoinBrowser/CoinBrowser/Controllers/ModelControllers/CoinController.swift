5ba095db53c8b27fcee35e976617ca5dc9bc92da
Colton Brenneman <colton.brenneman@gmail.com>
2023-06-12 14:39:49 -0600

    added a fetch function on our CoinController

--- CoinBrowser/CoinBrowser/Controllers/ModelControllers/CoinController.swift
+++ CoinBrowser/CoinBrowser/Controllers/ModelControllers/CoinController.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class CoinController {
+    
+    // MARK: - Properties
+    static var coins: [Coin] = []
+    private static let baseURLString = "https://api.coingecko.com/api/v3"
+    private static let keyCoinsComponent = "/coins"
+    private static let keyListComponent = "/list"
+    //finalURL https://api.coingecko.com/api/v3/coins/list
+    
+    // MARK: - CRUD
+    static func fetchCoins(completion: @escaping (Bool) -> Void) {
+        guard let baseURL = URL(string: baseURLString) else { return completion(false) }
+        let coinsURL = baseURL.appending(path: keyCoinsComponent)
+        let finalURL = coinsURL.appending(path: keyListComponent)
+        print(finalURL)
+    } //End of class
+}
--- CoinBrowser/CoinBrowser/Controllers/ViewControllers/CoinListTableViewController.swift
+++ CoinBrowser/CoinBrowser/Controllers/ViewControllers/CoinListTableViewController.swift
@@ -15,11 +15,6 @@ class CoinListTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
